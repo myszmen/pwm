e94641d0a60a5024fde2dd292d1cf1af4ccf28a3
Andrzej Szary <aszary@gmail.com>
2025-02-26 13:43:22 +0100

    using custom vnc port for multiple users

---
+++ scripts/ssh_tunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Run on local machine to enable VNC connection
+
+# Check if a port number is provided
+if [ -z "$1" ]; then
+	echo "Usage: $0 <port-number>"
+	exit 1
+fi
+
+PORT=$1
+
+ssh -L $PORT:kopernik:$PORT -J aszary@kepler.ia.uz.zgora.pl aszary@kopernik.ia.uz.zgora.pl
+
--- scripts/startx.sh
+++ scripts/startx.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Check if a port number is provided
+if [ -z "$1" ]; then
+	  echo "Usage: $0 <port-number>"
+	    exit 1
+fi
+
+PORT=$1
+
 # Start Xvfb in the background
 Xvfb :99 -screen 0 1024x768x24 > /home/psr/log/xvfb.log 2>&1 &
 
 sleep 2
 
-# Start x11vnc in the background
-x11vnc -display :99 -forever -nopw -rfbport 5900 > /home/psr/log/x11vnc.log 2>&1 &
+# Start x11vnc in the background with the specified port
+x11vnc -display :99 -forever -nopw -rfbport $PORT > /home/psr/log/x11vnc.log 2>&1 &
 
